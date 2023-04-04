ce2ea7adeba941fb28ee331e4bdf9007f5e5be2a
Aleksei <aleksei.kotliarov@epita.fr>
2023-04-04 12:27:47 +0200

    opti af2

--- input/testff/testPipeFF.c
+++ input/testff/testPipeFF.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <SDL2/SDL.h>
 #include <err.h>
 #include <dirent.h>
+#include <pthread.h>
 
  
 // Video resolution
@@ -22,7 +22,7 @@ typedef struct {
     char codec[20];
 } video_info;
 
-// get the number of frames with ffprobe
+
 
 void get_num_frames(char *filename, video_info *vi)
 {
@@ -57,26 +57,41 @@ video_info get_video_info(char *filename)
     pipein = popen(command, "r");
     fscanf(pipein, "%d", &info.height);
 
-    char *pref3 = "ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of csv=s=x:p=0 ";
+    // fps
+    char *pref3 = "ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of csv=s=x:p=0 ";
     strcpy(command, pref3);
-    strcat(command, filename);
-    pipein = popen(command, "r");
-    fscanf(pipein, "%s", info.codec);
-
-    char *pref4 = "ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of csv=s=x:p=0 ";
-    strcpy(command, pref4);
     strcat(command, filename);
     pipein = popen(command, "r");
     fscanf(pipein, "%d", &info.fps);
 
-    get_num_frames(filename, &info);
     pclose(pipein);
     return info;
 }
 
-void frame2ascii(unsigned char frame[H][W][3], char *asciis, int len, int tile_size)
+void frame2ascii(unsigned char frame[H][W][3], const char *asciis, const int len, const int tile_size)
 {
     int i, j, x, y, sum = 0;
+    for (y=0 ; y<H ; y += tile_size)
+    {
+        for (x=0 ; x<W ; x+=tile_size)
+        {
+            for (j = 0; j<tile_size; ++j) for (i = 0; i<tile_size; ++i)
+            {
+                if (y+j < H && x+i < W)
+                {
+                    sum += (frame[y+j][x+i][0] + frame[y+j][x+i][1] + frame[y+j][x+i][2])/3;
+                }
+            }
+            sum /= tile_size*tile_size;
+            printf("%c", asciis[sum*len/255]);
+        }
+        printf("\n");
+    }
+}
+
+void frame2ascii2(unsigned char frame[H][W][3], const char *asciis, const int len, const int tile_size)
+{
+    int i, j, x, y, sum;
     char ascii_char;
 
     for (y = 0; y < H; y += tile_size)
@@ -106,31 +121,24 @@ void frame2ascii(unsigned char frame[H][W][3], char *asciis, int len, int tile_s
     }
 }
 
-void read_video(char *filename, char *asciis, int len, int tile_size, int fps)
+
+
+void read_video(char cmd[1024], const char *asciis, const int len, const int tile_size, int fps)
 {
     int count;
     unsigned char frame[H][W][3];
+    //printf("%s", cmd);
 
-    //char res[H/tile_size * (W/tile_size+1)];
-
-    //char *post = " -f image2pipe -framerate 15 -vcodec h264 -pix_fmt rgb24 -";
-    // make a string to hold the command
-    char command[200];
-    char *pref = "ffmpeg -i ";
-    // copy the command into the string
-    strcpy(command, pref);
-    strcat(command, filename);
-    strcat(command, " -f image2pipe -framerate 20");
-    strcat(command, " -vcodec rawvideo -pix_fmt rgb24 -");
+    //har cmd[1024];
+    //snprintf(cmd, sizeof(cmd), "ffmpeg -f %s -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -", filename);
 
     // Open an input pipe from ffmpeg and an output pipe to a second instance of ffmpeg
-    FILE *pipein = popen(command, "r");
+    FILE *pipein = popen(cmd, "r");
 
 
     // Process video frames
     while(1)
     {
-        //memset(res, 0, (H/tile_size + 1) * W/tile_size);
 
         // Read a frame from the input pipe into the buffer
         count = fread(frame, 1, H*W*3, pipein);
@@ -138,35 +146,86 @@ void read_video(char *filename, char *asciis, int len, int tile_size, int fps)
         // If we didn't get a frame of video, we're probably at the end
         if (count != H*W*3) break;
 
-        system("clear");
-        frame2ascii(frame, asciis, len, tile_size);
-        //printf("%s", res);
-        //k++;
+        // clear the screen
+        printf("\033[2J\033[1;1H");
+        frame2ascii2(frame, asciis, len, tile_size);
         usleep(1000000/fps);
     }
     fflush(pipein);
     pclose(pipein);
 }
 
-
-
-
-
 int main(int argc, char *argv[])
 {
-    W = atoi(argv[4]);
-    H = atoi(argv[5]);
-    char *asciis = " .:-=+*#%@";
-    //char *asciis = " .,:ilwW";
-    int len = strlen(asciis);
-    int tile_size = atoi(argv[2]);
-    int fps = atoi(argv[3]);
+    if (argc < 2) {
+        printf("Usage:\nVideo: <video_file> <tile_size> <fps>\nWebcam: web_cam <tile_size>\nPhone cam: phone_cam <tile_size>\n");
+        return 0;
+    }
+    char cmd[1024];
+    const char *asciis = " .,:-=+*#%@";
+    const int len = strlen(asciis);
+    const int tile_size = atoi(argv[2]);
+    int fps = 60;
+    #if __APPLE__
+    {
+        if (strcmp(argv[1], "web_cam") == 0)
+        {
+            snprintf(cmd, sizeof(cmd), "ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i \"0\" -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -");
+            //printf("%s\n\n\n", cmd);
+            H = 720;
+            W = 1280;
+            read_video(cmd, asciis, len, tile_size, fps);
+        }
+        else if (strcmp(argv[1], "phone_cam") == 0)
+        {
+            snprintf(cmd, sizeof(cmd), "ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i \"1\" -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -");
+            H = 720;
+            W = 1280;
+            read_video(cmd, asciis, len, tile_size, fps);
+        }
+        else
+        {
+            video_info info = get_video_info(argv[1]);
+            H = info.height;
+            W = info.width;
+            fps = atoi(argv[3]);
+            snprintf(cmd, sizeof(cmd), "ffmpeg -i %s -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -", argv[1]);
+            read_video(cmd, asciis, len, tile_size, fps);
+        }
+    }
+    #elif _WIN32
+        // windows specific code
+    #elif __LINUX__
+    {
+        if (strcmp(argv[1], "web_cam") == 0)
+        {
+            snprintf(cmd, sizeof(cmd), "ffmpeg -i dev/video0 -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -");
+            //printf("%s\n\n\n", cmd);
+            H = 720;
+            W = 1280;
+            read_video(cmd, asciis, len, tile_size, fps);
+        }
+        else
+        {
+            video_info info = get_video_info(argv[1]);
+            H = info.height;
+            W = info.width;
+            fps = atoi(argv[3]);
+            snprintf(cmd, sizeof(cmd), "ffmpeg -i %s -f image2pipe -pix_fmt rgb24 -vcodec rawvideo -", argv[1]);
+            read_video(cmd, asciis, len, tile_size, fps);
+        }
+        // linux specific code
+    }
+    #elif BSD
+        // BSD specific code
+    #else
+        // general code or warning
+    #endif
 
-    // get the number of frames with ffprobe
+    //W = atoi(argv[4]);
+    //H = atoi(argv[5]);
 
-
-
-    read_video(argv[1], asciis, len, tile_size, fps);
+    //read_video(argv[1], asciis, len, tile_size, fps);
 
     return EXIT_SUCCESS;
 }
